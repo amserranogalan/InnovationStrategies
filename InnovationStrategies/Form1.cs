3d3f1d5abbfc5f7c1f6227406a444141881b9b22
amserranogalan <amserranogalan@gmail.com>
2020-02-21 09:04:30 +0100

    no message

---
+++ InnovationStrategies/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace InnovationStrategies
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            cmbNumberInfringement.DataSource = InfringementType.GetAll();
+            cmbNumberInfringement.ValueMember="number";
+            cmbNumberInfringement.DisplayMember = "infringement";
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            InfringementType it = new InfringementType();
+
+            it.Infringement=txtInfringement.Text;
+            it.Points=decimal.ToInt32(nudPoints.Value);
+
+            it.Add();
+
+            it=null;
+
+            System.Windows.Forms.MessageBox.Show("Registro grabado");
+
+            txtInfringement.Text = "";
+            nudPoints.Value=1;
+        }
+
+        private void btnOkDriver_Click(object sender, EventArgs e)
+        {
+            Drivers driver = new Drivers();
+
+            driver.Dni = txtDni.Text;
+            driver.Name = txtName.Text;
+            driver.Surnames = txtSurnames.Text;
+
+            if (driver.Exist())
+            {
+                System.Windows.Forms.MessageBox.Show("Ya existe un cliente con este DNI.");
+            }
+            else
+            {
+                driver.Add();
+                System.Windows.Forms.MessageBox.Show("Registro grabado");
+            }
+            
+            driver = null;
+
+            txtDni.Text = "";
+            txtName.Text = "";
+            txtSurnames.Text = "";
+        }
+
+        private void btnOkInfringement_Click(object sender, EventArgs e)
+        {
+            //Construimos la nueva infracción
+            InfringementDrivers id = new InfringementDrivers();
+
+            id.Date = DateTime.Now;
+            id.NumberInfringement = Convert.ToInt32(cmbNumberInfringement.SelectedValue.ToString());
+            id.VehicleRegistration = txtVehicle.Text;
+
+            //Usamos la matrícula del vehículo para rescatar los datos del conductor habitual
+            Vehicles v = new Vehicles(id.VehicleRegistration);
+
+            id.Dni = v.Dni;
+
+            id.Add();
+
+            //Construimos una clase conductor para ejecutar la sanción
+            Drivers d = new Drivers(id.Dni);
+
+            //Usamos el tipo de infracción introducido para rescatar los puntos de sanción de dicha infracción
+            InfringementType it = new InfringementType(id.NumberInfringement);
+
+            //Llamamos a la función que resta los puntos de ese tipo de infracción
+            d.Sanction(it.Points);
+
+            System.Windows.Forms.MessageBox.Show("Infracción introducida - el conductor:" + d.Name + " " + d.Surnames + " ha sido sancionado con: " + it.Points + " puntos.");
+
+            id = null;
+            d = null;
+            it = null;
+            v = null;
+
+            txtVehicle.Text = "";
+            cmbNumberInfringement.SelectedIndex = 0;
+        }
+
+        private void btnOkVehicles_Click(object sender, EventArgs e)
+        {
+            Vehicles v = new Vehicles();
+
+            v.VehicleRegistration=txtVehicleRegistration.Text;
+            v.Brand=txtBrand.Text;
+            v.Model=txtModel.Text;
+            v.Dni=txtDniConductorHabitual.Text;
+
+            if (v.Exist())
+            {
+                System.Windows.Forms.MessageBox.Show("Ya existe un vehículo con esa matrícula.");
+            }
+            else
+            {
+                if (!Drivers.Exist(v.Dni))
+                {
+                    System.Windows.Forms.MessageBox.Show("El DNI del conductor introducido no existe.");
+                }
+                else
+                {
+                    v.Add();
+                    System.Windows.Forms.MessageBox.Show("Registro grabado");
+                }
+            }
+            
+            v=null;
+
+            txtDniConductorHabitual.Text="";
+            txtBrand.Text="";
+            txtModel.Text="";
+            txtVehicleRegistration.Text="";
+        }
+    }
+}
---
+++ InnovationStrategies/InfringementDrivers.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace InnovationStrategies
+{
+    public class InfringementDrivers
+    {
+        private int _NumberInfringement;
+        private string _Dni;
+        private DateTime _Date;
+        private string _VehicleRegistration;
+        /// <summary>
+        /// Número de infracción
+        /// </summary>
+        public int NumberInfringement
+        {
+            get
+            {
+                return _NumberInfringement;
+            }
+            set
+            {
+                _NumberInfringement = value;
+            }
+        }
+
+        /// <summary>
+        /// Dni del conductor que ha cometido la infracción
+        /// </summary>
+        public string Dni
+        {
+            get
+            {
+                return _Dni;
+            }
+            set
+            {
+                _Dni = value;
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return _Date;
+            }
+            set
+            {
+                _Date = value;
+            }
+        }
+
+        /// <summary>
+        /// Matrícula del vehículo en el que se ha cometido la infracción
+        /// </summary>
+        public string VehicleRegistration
+        {
+            get
+            {
+                return _VehicleRegistration;
+            }
+            set
+            {
+                _VehicleRegistration = value;
+            }
+        }
+
+        /// <summary>
+        /// Añade una infracción y llama al metodo Drivers.Sanction para descontar los puntos correspondientes
+        /// </summary>
+        public void Add()
+        {
+            MySqlCommand cmd = InnovationStrategies.ConnectionBD.Cn.CreateCommand();
+
+            cmd.CommandText = "INSERT INTO infringementdrivers(date, dni, numberinfringement, vehicleregistration) VALUES ('" + Date.ToString("yyyy-MM-dd HH:mm") + "', '" + Dni + "', " + NumberInfringement + ", '" + VehicleRegistration + "')";
+
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
